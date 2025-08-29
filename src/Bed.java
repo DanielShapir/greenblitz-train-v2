6a89333d53eb711ee61fb20498966a2698deeaa6
daniel <danielshapir1@gmail.com>
2025-08-29 21:02:48 +0300

    question1

--- src/Bed.java
+++ src/Bed.java
@@ -1,5 +1,12 @@
 public class Bed {
-
+private int numOfPillows;
+private double height;
+private String colorOfSheets;
+public Bed(int numOfPillows,double height,String colorOfSheets){
+    this.colorOfSheets = colorOfSheets;
+    this.height = height;
+    this.numOfPillows = numOfPillows;
+}
 
 
 
