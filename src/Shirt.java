d356f5c3d967d73f79cc194676d5eeeedc39de9f
daniel <danielshapir1@gmail.com>
2025-08-29 21:44:06 +0300

    question6

---
+++ src/Shirt.java
@@ -0,0 +1,18 @@
+public class Shirt {
+    public int size;
+    public String color;
+
+    public Shirt(String color, int size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
