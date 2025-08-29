d6666ce146919cdc1436f910fb14e98217349e24
daniel <danielshapir1@gmail.com>
2025-08-29 21:05:15 +0300

    question2

--- src/Bed.java
+++ src/Bed.java
@@ -1,14 +1,22 @@
 public class Bed {
-private int numOfPillows;
-private double height;
-private String colorOfSheets;
-public Bed(int numOfPillows,double height,String colorOfSheets){
-    this.colorOfSheets = colorOfSheets;
-    this.height = height;
-    this.numOfPillows = numOfPillows;
-}
+    private int numOfPillows;
+    private double height;
+    private String colorOfSheets;
+
+    public Bed(int numOfPillows, double height, String colorOfSheets) {
+        this.colorOfSheets = colorOfSheets;
+        this.height = height;
+        this.numOfPillows = numOfPillows;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public int getNumOfPillows() {return numOfPillows;}
+    public String getColorOfSheets() {return colorOfSheets;}
 
 
 
-    
+
+
 }
