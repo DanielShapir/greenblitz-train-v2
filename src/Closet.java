afc34b3eeccc83951135e363eee3c8b5f78be1b0
daniel <danielshapir1@gmail.com>
2025-09-03 16:24:00 +0300

    question8

--- src/Closet.java
+++ src/Closet.java
@@ -1,11 +1,31 @@
 public class Closet {
     private Shirt[] shirts;
 
-public Closet (int numOfShelves){
-    this.shirts = new Shirt[numOfShelves];
-}
-public Shirt getShirt(int shelf ){
-    return new Shirt(shirts[shelf]);
-}
+    public Closet(int numOfShelves) {
+        this.shirts = new Shirt[numOfShelves];
+    }
 
+    public Shirt getShirt(int shelf) {
+        return new Shirt(shirts[shelf]);
+    }
+
+    public Shirt[] getShirtBySize(int size) {
+
+        int counter = 0;
+        for (int i = 0; i < shirts.length; i++) {
+            if (shirts[i] != null && shirts[i].getSize() == size)
+                counter++;
+        }
+        Shirt[] shirtsToReturn = new Shirt[counter];
+        int j = 0;
+        while (j < counter) {
+            if (shirts[j].getSize() == size) {
+                shirtsToReturn[j] = new Shirt(shirts[j]);
+                j++;
+            }
+        }
+        return shirtsToReturn;
+
+
+    }
 }
