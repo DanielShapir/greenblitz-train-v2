7b21236a088a1f8b823a0ba341d71efc7a25a20b
daniel <danielshapir1@gmail.com>
2025-09-04 23:38:53 +0300

    question5

--- src/lessonAboutRecords/Student.java
+++ src/lessonAboutRecords/Student.java
@@ -1,6 +1,11 @@
 package lessonAboutRecords;
 
 public record Student(String name , int grade , int classNum) {
+    public Student ( String name ,int grade ){
+        this(name,grade,9);
+    }
+
+
     public String getInfo(){
         String strToReturn = (name + " is in" + grade + "th and in class number : " + classNum );
         return strToReturn;
