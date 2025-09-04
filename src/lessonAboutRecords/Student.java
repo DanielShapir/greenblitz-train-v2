d9ccc831fadfdc8648ec8a3b420007bc237182cf
daniel <danielshapir1@gmail.com>
2025-09-04 23:21:43 +0300

    question 3 skipped2

--- src/lessonAboutRecords/Student.java
+++ src/lessonAboutRecords/Student.java
@@ -1,6 +1,9 @@
 package lessonAboutRecords;
 
 public record Student(String name , int grade) {
-
+    public String getInfo(){
+        String strToReturn = (name + " is in" + grade + "th");
+        return strToReturn;
+    }
 
 }
