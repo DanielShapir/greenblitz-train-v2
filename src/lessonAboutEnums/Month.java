84a0a77aafdf77814ded75aa507025bf00331089
daniel <danielshapir1@gmail.com>
2025-09-10 13:23:13 +0300

    question 2 herman im tired from checking the calender

--- src/lessonAboutEnums/Month.java
+++ src/lessonAboutEnums/Month.java
@@ -1,7 +1,24 @@
 package lessonAboutEnums;
 
 public enum Month {
+    JAN(31),
+    FEB(28),
+    MAR(31),
+    APR(30),
+    MAY(31),
+    JUN(30),
+    JUL(31),
+    AUG(31),
+    SEP(30),
+    OCT(30),
+    NOV(30),
+    DEC(31);
 
 
+    private int numOfDays;
+
+    Month(int getNumOfDays) {
+        this.numOfDays = numOfDays;
+    }
 
 }
