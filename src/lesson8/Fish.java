153e7fa7932b501880d952c2b39aadacb7ab5dac
daniel <danielshapir1@gmail.com>
2025-09-17 13:10:49 +0300

    Question 1

---
+++ src/lesson8/Fish.java
@@ -0,0 +1,52 @@
+package lesson8;
+
+public abstract class Fish {
+    private int age;
+    private String name;
+    private String favFood;
+
+    public Fish(String name, int age, String favFood) {
+        this.age = age;
+        this.name = name;
+        this.favFood = favFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFavFood() {
+        return favFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFavFood(String favFood) {
+        this.favFood = favFood;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void takeTheCar() {
+        System.out.println(" took the car");
+
+    }
+
+    public void Walk() {
+        System.out.println(" i walked");
+
+    }
+    public void takeTheTrain(){
+        System.out.println(" took the train");
+
+    }
+
+}
